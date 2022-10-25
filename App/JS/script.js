bf99a863dff2eed5ea46744ff90d64119862d966
NjimsRane <njims2011@gmail.com>
2022-10-26 00:33:31 +0100

    added cards

--- App/JS/script.js
+++ App/JS/script.js
@@ -1,6 +1,12 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navBar= document.querySelector(".nav-bar");
+const socialMedia= document.querySelector(".social-media-links"); 
+
 hamburgerMenu.addEventListener("click",function(){
     hamburgerMenu.classList.toggle("hamburger-menu-move");
-    navBar.classList.toggle('nav-bar-show')
-})
+    navBar.classList.toggle('nav-bar-show');
+    socialMedia.classList.toggle('social-media-links-hide');
+});
+
+
+
